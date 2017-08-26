c5da6297457052fa166f1f2f4e33d1024c25ef12
wangiegie <wangiegie@gmail.com>
2017-08-26 18:04:39 +0800

    【修改】 关于auth2.0的依赖

--- springcloud-security-oauth-resource/src/main/java/com/example/ResourceApplication.java
+++ springcloud-security-oauth-resource/src/main/java/com/example/ResourceApplication.java
@@ -3,10 +3,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
