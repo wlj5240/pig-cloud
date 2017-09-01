5985905982a7684e8d0cc0b185bd27ab4bdc491e
wangiegie <wangiegie@gmail.com>
2017-09-01 22:26:24 +0800

    【新增】 rabbitmq

---
+++ springboot-rabbitmq/src/main/java/com/example/sbmp/config/RabbitConfig.java
@@ -0,0 +1,20 @@
+package com.example.sbmp.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @email wangiegie@gmail.com
+ * @data 2017-09
+ */
+@Configuration
+public class RabbitConfig {
+
+//    @Bean
+//    public Queue helloQueue() {
+//        return new Queue("hello");
+//    }
+
+}
